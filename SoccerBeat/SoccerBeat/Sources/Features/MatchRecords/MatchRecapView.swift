fb66109adb3cd6340c4efd1c352050bffebd9ed8
Damagucci-Juice <50472122+Damagucci-Juice@users.noreply.github.com>
2024-04-04 22:40:11 +0900

    feat: delete HKWorkout data (#290)
    * feat: [#287] 삭제 기능 List + onDelete 로 구현
    
    * feat: [#287] Radar Chart Shape를 SwiftUI로 추가해서 뷰 크기를 조절
    
    * feat: HKWorkout을 삭제하는 기능 추가
    
    - 삭제하고 메인뷰로 갔다가 다시 들어오는 상황에서 많은 기다림이 요구됨
    - UI API Main Thread Checker, Publishing at Main Thread(ProfileModel) 등 많은 오류가 떠버림
    
    * feat: [#287] padding, 줄 간격 조정, 리스트 디바이더 제거
    
    * feat: [#287] HealthInteractor 안에서 들고 있는 내부 변수도 제거

--- SoccerBeat/SoccerBeat/Sources/Features/MatchRecords/MatchRecapView.swift
+++ SoccerBeat/SoccerBeat/Sources/Features/MatchRecords/MatchRecapView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct MatchRecapView: View {
+    @EnvironmentObject var healthInteractor: HealthInteractor
     @State private var userName = ""
-    let userWorkouts: [WorkoutData]
+    @Binding var userWorkouts: [WorkoutData]
 
     var body: some View {
         VStack(spacing: 0) {
@@ -44,21 +45,42 @@ struct MatchRecapView: View {
             }
             .padding(.leading, 32)
             
-            VStack(spacing: 15) {
+            List {
                 ForEach(userWorkouts) { workout in
-                    NavigationLink {
-                        MatchDetailView(workoutData: workout)
-                        .toolbarRole(.editor)
-                    } label: {
+                    ZStack {
+                        NavigationLink {
+                            MatchDetailView(workoutData: workout)
+                                .toolbarRole(.editor)
+                        } label: {
+                            EmptyView()
+                        }
+                        .opacity(0.0)
+                        
                         MatchListItemView(workoutData: workout)
+                            .buttonStyle(.plain)
+                    }
+                    .padding(.vertical, 2)
+                    .listRowSeparator(.hidden)
+                }
+                .onDelete { offset in
+                    Task {
+                        await delete(offset)
                     }
                 }
             }
-            .padding(.top, 61)
-            .padding(.horizontal, 16)
+            .listStyle(.plain)
         }
         .onAppear {
             userName = UserDefaults.standard.string(forKey: "userName") ?? ""
+        }
+    }
+    
+    private func delete(_ offset: IndexSet) async {
+        userWorkouts.remove(atOffsets: offset)
+        do {
+            try await healthInteractor.delete(at: offset)
+        } catch {
+            NSLog("Deleting HKWorkout failed")
         }
     }
 }
@@ -70,87 +92,107 @@ struct MatchListItemView: View {
     
     var body: some View {
         ZStack {
+            // 배경뷰
             LightRectangleView(alpha: 0.2, color: .white, radius: 15)
             
+            // 좌상단 뱃지뷰
             VStack {
                 HStack(spacing: 0) {
-                    ForEach(workoutData.matchBadge.indices, id: \.self) { index in
-                        if let badgeName = ShortenedBadgeImageDictionary[index][workoutData.matchBadge[index]] {
-                            if badgeName.isEmpty {
-                                EmptyView()
-                            } else {
-                                Image(badgeName)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 32, height: 36)
-                            }
-                        } else {
-                            EmptyView()
-                        }
-                    }
-                    .offset(CGSize(width: 15.0, height: -10.0))
+                    badges
+                        .offset(y: -12)
                     Spacer()
                 }
                 Spacer()
             }
             
-            HStack {
-                Spacer ()
-            
-                let recent = DataConverter.toLevels(workoutData)
-                let average = DataConverter.toLevels(profileModel.averageAbility)
+            HStack(spacing: 0) {
+                // 스파이더 차트
+                radarCharts
+                    .padding(.top, 16)
                 
-                ViewControllerContainer(ThumbnailViewController(radarAverageValue: average, radarAtypicalValue: recent))
-                    .scaleEffect(CGSize(width: 0.4, height: 0.4))
-                    .fixedSize()
-                    .frame(width: 88, height: 88)
- 
+                // 경기 데이터들
                 VStack(alignment: .leading) {
-                    Group {
-                        Text(workoutData.yearMonthDay.description + " - " + currentLocation)
-                            .task {
-                                currentLocation = await workoutData.location
-                            }
-                        Text("경기 시간 " + workoutData.time)
-                        
-                    }
-                    .opacity(0.6)
-                    .font(.matchDateLocationText)
+                    timeAndLocation
                     
                     Spacer()
                     
-                    HStack(spacing: 16) {
-                        VStack(alignment: .leading) {
-                            Text("뛴 거리")
-                            Text(String(format: "%.1f", workoutData.distance) + "km")
-                                .bold()
-                        }
-                        
-                        VStack(alignment: .leading) {
-                            Text("최고 속도")
-                            Text("\(Int(workoutData.velocity).formatted()) km/h")
-                                .bold()
-                        }
-                        
-                        VStack(alignment: .leading) {
-                            Text("스프린트 횟수")
-                            Text("\(workoutData.sprint) 회")
-                                .bold()
-                        }
-                    }
-                    .font(.system(size: 14))
-                    
+                    matchMatrics
                 }
+                .padding(.vertical, 8)
+                .frame(width: 225)
                 .foregroundStyle(.white)
-                .padding()
-                
-                Spacer()
             }
         }
-        .frame(height: 114)
+    }
+}
+
+extension MatchListItemView {
+    @ViewBuilder
+    var badges: some View {
+        ForEach(workoutData.matchBadge.indices, id: \.self) { index in
+            if let badgeName = ShortenedBadgeImageDictionary[index][workoutData.matchBadge[index]] {
+                if badgeName.isEmpty {
+                    EmptyView()
+                } else {
+                    Image(badgeName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 32, height: 36)
+                }
+            } else {
+                EmptyView()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    var radarCharts: some View {
+        let recent = DataConverter.toLevels(workoutData)
+        let average = DataConverter.toLevels(profileModel.averageAbility)
+        
+        RadarChartView(averageDataPoints: recent, maximumDataPoints: average, limitValue: 2.5)
+    }
+    
+    @ViewBuilder
+    var timeAndLocation: some View {
+        Group {
+            Text(workoutData.yearMonthDay.description + " - " + currentLocation)
+                .task {
+                    currentLocation = await workoutData.location
+                }
+            Text("경기 시간 " + workoutData.time)
+            
+        }
+        .opacity(0.6)
+        .font(.matchDateLocationText)
+    }
+    
+    @ViewBuilder
+    var matchMatrics: some View {
+        HStack(spacing: 16) {
+            VStack(alignment: .leading) {
+                Text("뛴 거리")
+                Text(String(format: "%.1f", workoutData.distance) + "km")
+                    .bold()
+            }
+            
+            VStack(alignment: .leading) {
+                Text("최고 속도")
+                Text("\(Int(workoutData.velocity).formatted()) km/h")
+                    .bold()
+            }
+            
+            VStack(alignment: .leading) {
+                Text("스프린트")
+                Text("\(workoutData.sprint) 회")
+                    .bold()
+            }
+        }
+        .padding(.vertical, 8)
+        .font(.system(size: 14))
     }
 }
 
 #Preview {
-    MatchRecapView(userWorkouts: WorkoutData.exampleWorkouts)
+    MatchRecapView(userWorkouts: .constant(WorkoutData.exampleWorkouts))
 }
