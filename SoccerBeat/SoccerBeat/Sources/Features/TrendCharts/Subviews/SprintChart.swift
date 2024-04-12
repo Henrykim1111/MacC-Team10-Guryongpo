cbdd357f9f686708c07e69a71e7636a4fd139bc8
Henrykim1111 <henrykim9319@gmail.com>
2024-04-13 00:30:16 +0900

    feat: [#295] BPM, SprintChart 횡스크롤 적용

--- SoccerBeat/SoccerBeat/Sources/Features/TrendCharts/Subviews/SprintChart.swift
+++ SoccerBeat/SoccerBeat/Sources/Features/TrendCharts/Subviews/SprintChart.swift
@@ -58,7 +58,7 @@ struct SprintChart: View {
     let fastestWorkout: WorkoutData
     let slowestWorkout: WorkoutData
     let averageSprint: Double
-    let betweenBarSpace: CGFloat = 65
+    let betweenBarSpace: CGFloat = 70
     
     private func isMax(_ workout: WorkoutData) -> Bool {
         workout == fastestWorkout
@@ -69,44 +69,92 @@ struct SprintChart: View {
     }
     
     var body: some View {
-        Chart {
-            ForEach(0..<workouts.count, id: \.self) { index in
-                let workout = workouts[index]
-                
-                BarMark(
-                    x: .value("Order", index),
-                    yStart: .value("Sprint", 0),
-                    yEnd: .value("Sprint", workout.sprint)
-                )
-                .foregroundStyle(isMax(workout) ? .sprintMax
-                                 : (isMin(workout) ? .sprintMin : .chartDefault))
-                .cornerRadius(300, style: .continuous)
-                // MARK: - Bar Chart Data, value 표시
-                // MARK: - 가장 밑에 일자 표시, 실제 보이는 용
-                .annotation(position: .bottom, alignment: .center) {
-                    let isMaxOrMin = isMin(workout) || isMax(workout)
-                    VStack(spacing: 6) {
-                        Text(isMaxOrMin ? workout.sprint.formatted() + "회" : "0회" )
-                            .font(.maxValueUint)
-                            .foregroundStyle(.maxValueStyle)
-                            .opacity(isMaxOrMin ? 1.0 : 0.0)
-                            .padding(.top, 8)
+        if #available(iOS 17.0, *) {
+            ScrollView(.horizontal, showsIndicators: false) {
+                Chart {
+                    ForEach(0..<workouts.count, id: \.self) { index in
+                        let workout = workouts[index]
                         
-                        Text("\(workout.day)일")
-                            .font(isMaxOrMin ? .maxDayUnit : .defaultDayUnit)
-                            .foregroundStyle(.defaultDayStyle)
+                        BarMark(
+                            x: .value("Order", index),
+                            yStart: .value("Sprint", 0),
+                            yEnd: .value("Sprint", workout.sprint)
+                        )
+                        .foregroundStyle(isMax(workout) ? .sprintMax
+                                         : (isMin(workout) ? .sprintMin : .chartDefault))
+                        .cornerRadius(300, style: .continuous)
+                        // MARK: - Bar Chart Data, value 표시
+                        // MARK: - 가장 밑에 일자 표시, 실제 보이는 용
+                        .annotation(position: .bottom, alignment: .center) {
+                            let isMaxOrMin = isMin(workout) || isMax(workout)
+                            VStack(spacing: 6) {
+                                Text(workout.sprint.formatted() + "회")
+                                    .font(.maxValueUint)
+                                    .foregroundStyle(.maxValueStyle)
+                                    .opacity(isMaxOrMin ? 1.0 : 0.5)
+                                    .padding(.top, 8)
+                                
+                                Text("\(workout.day)일")
+                                    .font(isMaxOrMin ? .maxDayUnit : .defaultDayUnit)
+                                    .foregroundStyle(.defaultDayStyle)
+                            }
+                        }
                     }
                 }
+                // MARK: - 가장 밑에 일자 표시, 자리잡기용
+                .chartXAxis {
+                    AxisMarks(values: .stride(by: .day)) { _ in
+                        AxisValueLabel(format: .dateTime.day(), centered: true)
+                            .font(.defaultDayUnit)
+                    }
+                }
+                .chartYAxis(.hidden)
+                .frame(width: CGFloat(workouts.count) * betweenBarSpace)
+            }
+            .defaultScrollAnchor(.trailing)
+        } else {
+            ScrollView(.horizontal, showsIndicators: false) {
+                Chart {
+                    ForEach(0..<workouts.count, id: \.self) { index in
+                        let workout = workouts[index]
+                        
+                        BarMark(
+                            x: .value("Order", index),
+                            yStart: .value("Sprint", 0),
+                            yEnd: .value("Sprint", workout.sprint)
+                        )
+                        .foregroundStyle(isMax(workout) ? .sprintMax
+                                         : (isMin(workout) ? .sprintMin : .chartDefault))
+                        .cornerRadius(300, style: .continuous)
+                        // MARK: - Bar Chart Data, value 표시
+                        // MARK: - 가장 밑에 일자 표시, 실제 보이는 용
+                        .annotation(position: .bottom, alignment: .center) {
+                            let isMaxOrMin = isMin(workout) || isMax(workout)
+                            VStack(spacing: 6) {
+                                Text(workout.sprint.formatted() + "회")
+                                    .font(.maxValueUint)
+                                    .foregroundStyle(.maxValueStyle)
+                                    .opacity(isMaxOrMin ? 1.0 : 0.5)
+                                    .padding(.top, 8)
+                                
+                                Text("\(workout.day)일")
+                                    .font(isMaxOrMin ? .maxDayUnit : .defaultDayUnit)
+                                    .foregroundStyle(.defaultDayStyle)
+                            }
+                        }
+                    }
+                }
+                // MARK: - 가장 밑에 일자 표시, 자리잡기용
+                .chartXAxis {
+                    AxisMarks(values: .stride(by: .day)) { _ in
+                        AxisValueLabel(format: .dateTime.day(), centered: true)
+                            .font(.defaultDayUnit)
+                    }
+                }
+                .chartYAxis(.hidden)
+                .frame(width: CGFloat(workouts.count) * betweenBarSpace)
             }
         }
-        // MARK: - 가장 밑에 일자 표시, 자리잡기용
-        .chartXAxis {
-            AxisMarks(values: .stride(by: .day)) { _ in
-                AxisValueLabel(format: .dateTime.day(), centered: true)
-                    .font(.defaultDayUnit)
-            }
-        }
-        .chartYAxis(.hidden)
     }
 }
 
