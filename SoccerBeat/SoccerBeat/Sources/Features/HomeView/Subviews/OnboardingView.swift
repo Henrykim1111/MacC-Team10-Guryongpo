397bfc19fb54ccc863f90d80093168134d171165
Henrykim1111 <henrykim9319@gmail.com>
2024-07-15 01:24:07 +0900

    feat: [#340] dismiss button added

--- SoccerBeat/SoccerBeat/Sources/Features/HomeView/Subviews/OnboardingView.swift
+++ SoccerBeat/SoccerBeat/Sources/Features/HomeView/Subviews/OnboardingView.swift
@@ -8,26 +8,38 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @Environment(\.dismiss) var dismiss
     var body: some View {
         VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "x.circle.fill")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 20))
+                }
+                
+            }
+            Spacer()
             Text(LocalizedStringKey("현재 기록된 경기가 없습니다."))
                 .font(.onboardingTop)
                 .foregroundStyle(.onboardingText)
-            Spacer()
-                .frame(height: 30)
             Image("OnboardingWatchImage")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 180)
-            Spacer()
-                .frame(height: 40)
             Text(LocalizedStringKey("애플워치에서 사커비트를 켜서"))
                 .font(.onboardingBottom)
                 .foregroundStyle(.playTimeNumber)
             Text(LocalizedStringKey("첫 경기를 기록하세요!"))
                 .font(.onboardingBottom)
                 .foregroundStyle(.onboardingText)
+            Spacer()
         }
+        .padding()
     }
 }
 
