083e3f553ca8b641e42b79d72c7c5d6710457a0c
Damagucci-Juice <jsr9874@gmail.com>
2024-07-20 11:58:51 +0900

    refactor: HealthInteractor와 SoccerBeatApp에서 SwiftLint 오류, print문, 논리상 No가 들어가 있는 변수 변경

--- SoccerBeat/Common/Data/HealthInteractor.swift
+++ SoccerBeat/Common/Data/HealthInteractor.swift
@@ -10,29 +10,33 @@ import CoreLocation
 import HealthKit
 import SwiftUI
 
+enum HealthKitError: Error {
+    case failureConvertingRouteAndMeta
+}
+
 final class HealthInteractor: NSObject, ObservableObject {
     // Object to request permission to read HealthKit data.
     var healthStore = HKHealthStore()
     let locationManager = CLLocationManager()
     let typesToRead = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!,
-                          HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
-                          HKObjectType.quantityType(forIdentifier: .runningSpeed)!,
-                          HKObjectType.quantityType(forIdentifier: .walkingSpeed)!,
-                          HKSeriesType.workoutType(),
-                          HKSeriesType.workoutRoute(),
-                          HKObjectType.activitySummaryType()
-                         ])
-    
+                           HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+                           HKObjectType.quantityType(forIdentifier: .runningSpeed)!,
+                           HKObjectType.quantityType(forIdentifier: .walkingSpeed)!,
+                           HKSeriesType.workoutType(),
+                           HKSeriesType.workoutRoute(),
+                           HKObjectType.activitySummaryType()
+                          ])
+
     let typesToShare: Set = [HKQuantityType.workoutType(),
-                                     HKSeriesType.workoutRoute(),
-                                     HKQuantityType.quantityType(forIdentifier: .runningSpeed)!,
-                                     HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!
-                                     
+                             HKSeriesType.workoutRoute(),
+                             HKQuantityType.quantityType(forIdentifier: .runningSpeed)!,
+                             HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!
+
     ]
-    
+
     // Entire user workouts in HealthKit data.
     private var hkWorkouts = [HKWorkout]()
-    
+
     override init() {
         super.init()
         locationManager.delegate = self
@@ -42,52 +46,65 @@ final class HealthInteractor: NSObject, ObservableObject {
     var authSuccess = PassthroughSubject<(), Never>()
     // Send when data fetch is successful.
     var fetchWorkoutsSuccess = PassthroughSubject<([WorkoutData]), Never>()
-    
+
     static let shared = HealthInteractor()
-    
+
     private let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         return formatter
     }()
-    
+
     @Published var recentGames = [WorkoutData]()
     @Published var recent4Games = [WorkoutData]()
-    
+
     private(set) var monthly = [String: [WorkoutData]]()
-    
-    func haveNoLocationAuthorization() -> Bool {
-        return locationManager.authorizationStatus == .denied
+
+    func hasLocationAuthorization() -> Bool {
+        [
+            CLAuthorizationStatus.authorizedAlways,
+            .authorizedWhenInUse
+        ].contains(locationManager.authorizationStatus)
     }
-    
-    func haveNoHealthAuthorization() -> Bool {
-        for type in typesToShare {
-            if healthStore.authorizationStatus(for: type) == .sharingDenied {
-                print(type, healthStore.authorizationStatus(for: type).rawValue)
-                return true
-            }}
-        return false
+
+    func haveHealthAuthorization() -> Bool {
+        for type in typesToShare
+        where healthStore.authorizationStatus(for: type) == .sharingDenied {
+            NSLog(
+                type.debugDescription,
+                healthStore.authorizationStatus(for: type).rawValue
+            )
+            return false
+        }
+        return true
     }
-    
+
     @MainActor
     func requestAuthorization() {
-        print("requestAuthorization: request user authorization..")
-        
-        // 휴대폰에서 위치 권한 얻기
-        if locationManager.authorizationStatus == .denied ||
-            locationManager.authorizationStatus == .notDetermined ||
-            locationManager.authorizationStatus == .restricted {
+        NSLog("requestAuthorization: request user authorization..")
+
+        let locationAccessDenied = [
+            CLAuthorizationStatus.notDetermined,
+            .denied,
+            .restricted
+        ]
+            .contains(locationManager.authorizationStatus)
+
+        if locationAccessDenied {
             self.locationManager.requestAlwaysAuthorization()
         }
-        
         // 해당 기기가 헬스킷을 사용할 수 있는지 확인 함
         guard HKHealthStore.isHealthDataAvailable() else {
-            print("requestAuthorization: health data not available")
+            NSLog("requestAuthorization: health data not available")
             return
         }
-        
+
         healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { success, error in
-            if success && !self.haveNoHealthAuthorization() {
+            guard let error else {
+                NSLog(error.debugDescription)
+                return
+            }
+            if success && self.haveHealthAuthorization() {
                 DispatchQueue.main.async {
                     self.authSuccess.send()
                 }
@@ -96,31 +113,36 @@ final class HealthInteractor: NSObject, ObservableObject {
             }
         }
     }
-    
+
     func delete(at offset: IndexSet) async throws {
         for index in offset {
             try await healthStore.delete(hkWorkouts[index])
         }
         hkWorkouts.remove(atOffsets: offset)
     }
-    
+
     @Published var isLoading = false
-    
+
     func fetchWorkoutData() async {
         await MainActor.run {
             isLoading = true
         }
-        
+
         // Fetch from HealthStore
         self.hkWorkouts = await fetchHKWorkouts()
-        
+
         // Convert WorkoutData(Bussiness Model)
         var workoutData = [WorkoutData]()
         for (index, workout) in self.hkWorkouts.enumerated() {
-            let workoutDatum = await convert(from: workout, at: index)
-            workoutData.append(workoutDatum)
+            do {
+                let workoutDatum = try await convert(from: workout, at: index)
+                workoutData.append(workoutDatum)
+            } catch {
+                NSLog(error.localizedDescription)
+                continue
+            }
         }
-        
+
         await settingForChartView(workoutData)
         monthly = divideWorkoutsByMonthly(workoutData)
 
@@ -129,13 +151,18 @@ final class HealthInteractor: NSObject, ObservableObject {
             self.fetchWorkoutsSuccess.send(workoutData)
         }
     }
-    
-    private func convert(from workout: HKWorkout, at index: Int) async -> WorkoutData {
-        
+
+    private func convert(from workout: HKWorkout, at index: Int) async throws -> WorkoutData {
+
         var latSum = 0.0
         var lonSum = 0.0
         var routes: [CLLocationCoordinate2D] = []
-        let (locations, metadata) = await convertToRouteAndMetadata(from: workout)
+
+        guard let (locations, metadata) = try? await convertToRouteAndMetadata(from: workout) else {
+            NSLog("Failure Converting Workout to Route and Metadata")
+            throw HealthKitError.failureConvertingRouteAndMeta
+        }
+
         for location in locations {
             routes.append(CLLocationCoordinate2D(latitude: location.coordinate.latitude,
                                                  longitude: location.coordinate.longitude))
@@ -143,44 +170,44 @@ final class HealthInteractor: NSObject, ObservableObject {
             lonSum += location.coordinate.longitude
         }
         let displayedTime = String(Int(workout.duration)/60) + " : " + String(Int(workout.duration) % 60)
-        var dotCount = routes.isEmpty ? 1 : routes.count
-        
+        let dotCount = routes.isEmpty ? 1 : routes.count
+
         // Metadata를 WorkoutData로 변환
         // 기본으로 데이터 오류 없음 가정
         var dataError = false
-        
+
         var distance = 0.0
         var sprintCount = 0
         var velocity = 0.0
         var power = 0.0
         var maxHeartRate = 0
         var minHeartRate = 0
-        
+
         if let distanceMeta: Double = metadata.getValue(forKey: "Distance") {
             distance = distanceMeta
         } else { dataError = true }
-        
+
         if let sprintCountMeta: Int = metadata.getValue(forKey: "SprintCount") {
             sprintCount = sprintCountMeta
         } else { dataError = true }
-        
+
         if let velocityMeta: Double = metadata.getValue(forKey: "MaxSpeed") {
             velocity = Double(((velocityMeta) * 3.6).rounded(at: 2)) ?? 0
         } else { dataError = true }
-        
-        if let powerMeta: Double = metadata.getValue(forKey: "Power") ?? 
+
+        if let powerMeta: Double = metadata.getValue(forKey: "Power") ??
             metadata.getValue(forKey: "Acceleration") {
-          power = powerMeta
+            power = powerMeta
         } else { dataError = true }
-        
+
         if let maxHeartRateMeta: Int = metadata.getValue(forKey: "MaxHeartRate") {
             maxHeartRate = maxHeartRateMeta
         } else { dataError = true }
-        
+
         if let minHeartRateMeta: Int = metadata.getValue(forKey: "MinHeartRate") {
             minHeartRate = minHeartRateMeta
         } else { dataError = true }
-        
+
         return WorkoutData(dataID: index+1,
                            date: dateFormatter.string(from: workout.startDate),
                            time: displayedTime,
@@ -197,60 +224,75 @@ final class HealthInteractor: NSObject, ObservableObject {
     }
 
     private func fetchHKWorkouts() async -> [HKWorkout] {
-        let soccer = HKQuery.predicateForObjects(from: .default())
-        let data = try? await withCheckedThrowingContinuation { (continuation: CheckedContinuation<[HKSample], Error>) in
-            healthStore.execute(HKSampleQuery(sampleType: .workoutType(), predicate: soccer, limit: HKObjectQueryNoLimit,sortDescriptors: [.init(keyPath: \HKSample.startDate, ascending: false)], resultsHandler: { query, samples, error in
-                if let hasError = error {
-                    continuation.resume(throwing: hasError)
-                    return
+        let soccerPredicate = HKQuery.predicateForObjects(from: .default())
+        let data = try? await withCheckedThrowingContinuation { (
+            continuation: CheckedContinuation<[HKSample], Error>
+        ) in
+            let query = HKSampleQuery(
+                sampleType: .workoutType(),
+                predicate: soccerPredicate,
+                limit: HKObjectQueryNoLimit,
+                sortDescriptors: [NSSortDescriptor(keyPath: \HKSample.startDate, ascending: false)],
+                resultsHandler: { _, samples, error in
+                    if let error = error {
+                        continuation.resume(throwing: error)
+                    } else if let samples = samples {
+                        continuation.resume(returning: samples)
+                    }
                 }
-                continuation.resume(returning: samples!)
-            }))
+            )
+            healthStore.execute(query)
         }
         guard let workouts = data as? [HKWorkout] else { return [] }
         return workouts
     }
 
     // HKWorkout + Metadata
-    // TODO: - throws로 바꿔야 할 것
-    // TODO: - withCheckedThrowingContinuation을 어떻게 바꿀것인가?
-    func convertToRouteAndMetadata(from workout: HKWorkout) async -> ([CLLocation], [String: Any]) {
-        let byWorkout = HKQuery.predicateForObjects(from: workout)
+    func convertToRouteAndMetadata(from workout: HKWorkout) async throws -> ([CLLocation], [String: Any]) {
+        let byWorkoutPredicate = HKQuery.predicateForObjects(from: workout)
 
-        let samples = try? await withCheckedThrowingContinuation { (continuation: CheckedContinuation<[HKSample], Error>) in
-            healthStore.execute(HKAnchoredObjectQuery(type: HKSeriesType.workoutRoute(), predicate: byWorkout, anchor: nil, limit: HKObjectQueryNoLimit, resultsHandler: { (query, samples, deletedObjects, anchor, error) in
-                if let hasError = error {
-                    continuation.resume(throwing: hasError); return
+        let samples = try await withCheckedThrowingContinuation { (
+            continuation: CheckedContinuation<[HKSample], Error>
+        ) in
+            let query = HKAnchoredObjectQuery(
+                type: HKSeriesType.workoutRoute(),
+                predicate: byWorkoutPredicate,
+                anchor: nil,
+                limit: HKObjectQueryNoLimit,
+                resultsHandler: { query, samples, deletedObjects, anchor, error in
+                    if let error = error {
+                        continuation.resume(throwing: error)
+                    } else if let samples = samples {
+                        continuation.resume(returning: samples)
+                    }
                 }
-
-                guard let samples = samples else { return }
-
-                continuation.resume(returning: samples)
-            }))
+            )
+            healthStore.execute(query)
         }
-        
-        guard let route = (samples as? [HKWorkoutRoute])?.first else { return ([], [:]) }
-        guard let metadata = route.metadata else { return ([], [:]) }
-        
-        let locations = try? await withCheckedThrowingContinuation { (continuation: CheckedContinuation<[CLLocation], Error>) in
-            var allLocations = [CLLocation]() // built up over time as and when HK tells us
-            healthStore.execute(HKWorkoutRouteQuery(route: route) { (query, locationsOrNil, done, errorOrNil) in
-                // This block may be called multiple times.
+
+        guard let route = (samples as? [HKWorkoutRoute])?.first, let metadata = route.metadata else {
+            return ([], [:])
+        }
+
+        let locations = try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<[CLLocation], Error>) in
+            var allLocations = [CLLocation]()
+            let query = HKWorkoutRouteQuery(route: route) { (query, locationsOrNil, done, errorOrNil) in
                 if let error = errorOrNil {
-                    continuation.resume(throwing: error); return
+                    continuation.resume(throwing: error)
+                    return
                 }
 
-                guard let locations = locationsOrNil else {
-                    fatalError("Invalid State: This can only fail if there was an error.")
+                if let locations = locationsOrNil {
+                    allLocations += locations
                 }
-                allLocations += locations
 
                 if done {
                     continuation.resume(returning: allLocations)
                 }
-            })
+            }
+            healthStore.execute(query)
         }
-        guard let locations else { return ([], [:]) }
+
         return (locations, metadata)
     }
 }
@@ -260,75 +302,55 @@ final class HealthInteractor: NSObject, ObservableObject {
 extension HealthInteractor {
 
     private func settingForChartView(_ workouts: [WorkoutData]) async {
-        let fourGames: [WorkoutData] = {
-            let games = readRecentMatches(with: workouts, for: 4)
-            let sorted = sortWorkoutsForChart(games)
-            return makeBlankWorkouts(with: sorted)
-        }()
-
+        let fourGames = await MainActor.run {
+            readRecentMatches(from: workouts, count: 4)
+                .sorted(by: { $0.formattedDate < $1.formattedDate })
+                .appendingBlanks(upTo: 4, with: .blankExample)
+        }
         await MainActor.run {
             recent4Games = fourGames
-            recentGames = sortWorkoutsForChart(workouts)
+            recentGames = workouts.sorted(by: { $0.formattedDate < $1.formattedDate })
         }
     }
-    
-    private func readRecentMatches(with workouts: [WorkoutData], for count: Int) -> [WorkoutData] {
+
+    private func readRecentMatches(from workouts: [WorkoutData], count: Int) -> [WorkoutData] {
         guard !workouts.isEmpty else { return [] }
-        guard workouts.count >= count  else { return workouts }
-        let startIndex = workouts.count - count
-        let lastIndex = workouts.count-1
-        var recentMatches = [WorkoutData]()
-        for i in startIndex...lastIndex {
-            recentMatches.append(workouts[i])
-        }
-        return recentMatches
+        return Array(workouts.suffix(count))
     }
-    
-    // 추세 데이터에서 데이터가 4개 이하인 경우에 실제 데이터와 fake 데이터를 혼합해서 보여줌
-    private func makeBlankWorkouts(with workouts: [WorkoutData]) -> [WorkoutData] {
-        var blanks = [WorkoutData]()
-        if workouts.count < 4 {
-            let count = workouts.count
-            let blankCount = 4-count
-            for _ in 0..<blankCount {
-                blanks.append(WorkoutData.blankExample)
-            }
-        }
-        return blanks + workouts
-    }
-    
-    private func sortWorkoutsForChart(_ workouts: [WorkoutData]) -> [WorkoutData] {
-        return workouts.sorted { preWork, postWork in
-            preWork.formattedDate < postWork.formattedDate
-        }
-    }
-    
+
     private func divideWorkoutsByMonthly(_ workouts: [WorkoutData]) -> [String: [WorkoutData]] {
-        var dict = [String: [WorkoutData]]()
-        workouts.forEach { match in
-                let yearMonth = Array(match.date.split(separator: "-")[...1]).joined(separator: "-")
-                dict[yearMonth, default: []].append(match)
-            }
-        return dict
+        return Dictionary(grouping: workouts) {
+            String($0.date.prefix(7))  // Assuming date format is "YYYY-MM-DD"
+        }
     }
-    
 }
+
+private extension Array where Element == WorkoutData {
+    func appendingBlanks(upTo count: Int, with blank: WorkoutData) -> [WorkoutData] {
+        let blankCount = Swift.max(0, count - self.count)
+        return Array(repeating: blank, count: blankCount) + self
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
 
 extension HealthInteractor: CLLocationManagerDelegate {
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let statusMessage: String
         switch manager.authorizationStatus {
         case .authorizedAlways:
-            print("항상 허용")
+            statusMessage = "항상 허용"
         case .notDetermined:
-            print("not decision")
+            statusMessage = "not decision"
         case .restricted:
-            print("ask later")
+            statusMessage = "ask later"
         case .denied:
-            print("denied")
+            statusMessage = "denied"
         case .authorizedWhenInUse:
-            print("when in use")
+            statusMessage = "when in use"
         @unknown default:
-            print("default")
+            statusMessage = "default"
         }
+        NSLog(statusMessage)
     }
 }
--- SoccerBeat/SoccerBeat/SoccerBeatApp.swift
+++ SoccerBeat/SoccerBeat/SoccerBeatApp.swift
@@ -12,25 +12,22 @@ struct SoccerBeatApp: App {
     @StateObject var soundManager = SoundManager()
     @StateObject var healthInteracter = HealthInteractor.shared
     @StateObject var profileModel = ProfileModel(healthInteractor: HealthInteractor.shared)
-    @State private var noHealth: Bool
-    @State private var noLocation: Bool
+    @State private var hasHealthAuthorization: Bool
+    @State private var hasLocationAuthorization: Bool
     
     init() {
-        self.noHealth = HealthInteractor.shared.haveNoHealthAuthorization()
-        self.noLocation = HealthInteractor.shared.haveNoLocationAuthorization()
+        self.hasHealthAuthorization = HealthInteractor.shared.haveHealthAuthorization()
+        self.hasLocationAuthorization = HealthInteractor.shared.hasLocationAuthorization()
     }
     var body: some Scene {
         WindowGroup {
-            //             if Authorization == sharingDenied, 명시적으로 거절함.
             Group {
-                if noHealth {
-                    NoAuthorizationView(requestingAuth: .health)
-                } else if noLocation {
-                    NoAuthorizationView(requestingAuth: .location)
-                }
-                
-                if !noHealth && !noLocation {
+                if hasHealthAuthorization && hasLocationAuthorization {
                     ContentView()
+                } else if !hasHealthAuthorization {
+                    NoAuthorizationView(requestingAuth: .health)
+                } else if !hasLocationAuthorization {
+                    NoAuthorizationView(requestingAuth: .location)
                 }
             }
             .environmentObject(soundManager)
@@ -43,10 +40,10 @@ struct SoccerBeatApp: App {
                         for: UIApplication.didBecomeActiveNotification
                     )
             ) { _ in
-                noHealth = healthInteracter.haveNoHealthAuthorization()
-                noLocation = healthInteracter.haveNoLocationAuthorization()
+                hasHealthAuthorization = healthInteracter.haveHealthAuthorization()
+                hasLocationAuthorization = healthInteracter.hasLocationAuthorization()
                 Task {
-                    if !noHealth && !noLocation {
+                    if hasHealthAuthorization && hasLocationAuthorization {
                         await self.healthInteracter.fetchWorkoutData()
                     }
                 }
