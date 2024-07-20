955201c058ca60c5827929928a720719b9451e65
Damagucci-Juice <jsr9874@gmail.com>
2024-07-20 12:47:11 +0900

    feat: OnBoardingView 지역화 구현

--- SoccerBeat/SoccerBeat/Sources/Features/HomeView/Subviews/OnboardingView.swift
+++ SoccerBeat/SoccerBeat/Sources/Features/HomeView/Subviews/OnboardingView.swift
@@ -11,33 +11,16 @@ struct OnboardingView: View {
     @Environment(\.dismiss) var dismiss
     var body: some View {
         VStack {
-            Spacer()
-            HStack {
-                Spacer()
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "x.circle.fill")
-                        .foregroundStyle(.white)
-                        .font(.system(size: 20))
-                }
-                
-            }
-            Spacer()
-            Text(LocalizedStringKey("현재 기록된 경기가 없습니다."))
+            Text("workout_empty")
                 .font(.onboardingTop)
                 .foregroundStyle(.onboardingText)
             Image("OnboardingWatchImage")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 180)
-            Text(LocalizedStringKey("애플워치에서 사커비트를 켜서"))
-                .font(.onboardingBottom)
-                .foregroundStyle(.playTimeNumber)
-            Text(LocalizedStringKey("첫 경기를 기록하세요!"))
+            Text("try_game_in_watch")
                 .font(.onboardingBottom)
                 .foregroundStyle(.onboardingText)
-            Spacer()
         }
         .padding()
     }
