f0cb990f0029156f8f0ffb4977c6a792fdc586f9
Jose <dbswkgus99@snu.ac.kr>
2024-08-22 22:45:39 +0900

    feat: [#394] Temporal Caption

--- SoccerBeat/SoccerBeat Watch App/Sources/WorkoutManager/MatricsIndicator.swift
+++ SoccerBeat/SoccerBeat Watch App/Sources/WorkoutManager/MatricsIndicator.swift
@@ -24,7 +24,6 @@ final class MatricsIndicator: NSObject, ObservableObject {
             
             if heartRate != 0 {
                 self.saveHeartRates.append(Int(heartRate))
-                print(saveHeartRates)
             }
         }
     }
@@ -168,8 +167,9 @@ final class MatricsIndicator: NSObject, ObservableObject {
                 self.calculateMaxPower(before: oldPower, current: self.power)
             case HKQuantityType.quantityType(forIdentifier: .vo2Max):
                 let oldvo2Max = self.vo2Max
-                self.vo2Max = statistics.mostRecentQuantity()?.doubleValue(for: HKUnit(from: "ml/kg*min")) ?? 0
+                self.vo2Max = statistics.mostRecentQuantity()?.doubleValue(for: HKUnit(from: "ml/(kg*min)")) ?? 0
                 self.calculateMaxVo2Max(before: oldvo2Max, current: self.vo2Max)
+//                print("vo2max", self.vo2Max)
             default:
                 return
             }
