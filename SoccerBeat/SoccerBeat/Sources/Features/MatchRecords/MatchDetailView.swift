73b2d9b4187c1e221cb9b574e2cb09705be306a7
Henrykim1111 <henrykim9319@gmail.com>
2024-08-25 00:07:28 -0400

    feat: [#402] 버전 업데이트를 위한 칼로리, VO2 주석처리

--- SoccerBeat/SoccerBeat/Sources/Features/MatchRecords/MatchDetailView.swift
+++ SoccerBeat/SoccerBeat/Sources/Features/MatchRecords/MatchDetailView.swift
@@ -332,20 +332,20 @@ struct FieldRecordDataView: View {
                         }
                     }
                     
-                    VStack(alignment: .leading) {
-                        Text("칼로리")
-                            .font(.fieldRecordTitle)
-                        HStack(alignment: .bottom, spacing: 0) {
-                            if let workout = workout {
-                                Text(workout.error ? "--" : workout.calories.formatted())
-                                    .font(.fieldRecordMeasure)
-                            } else {
-                                Text("--")
-                            }
-                            Text(" kcal")
-                                .font(.fieldRecordUnit)
-                        }
-                    }
+//                    VStack(alignment: .leading) {
+//                        Text("칼로리")
+//                            .font(.fieldRecordTitle)
+//                        HStack(alignment: .bottom, spacing: 0) {
+//                            if let workout = workout {
+//                                Text(workout.error ? "--" : workout.calories.formatted())
+//                                    .font(.fieldRecordMeasure)
+//                            } else {
+//                                Text("--")
+//                            }
+//                            Text(" kcal")
+//                                .font(.fieldRecordUnit)
+//                        }
+//                    }
                 }
                 
                 VStack(alignment: .leading, spacing: 24) {
@@ -393,20 +393,20 @@ struct FieldRecordDataView: View {
                         }
                     }
                     
-                    VStack(alignment: .leading) {
-                        Text("최대 산소 섭취량")
-                            .font(.fieldRecordTitle)
-                        HStack(alignment: .bottom, spacing: 0) {
-                            if let workout = workout {
-                                Text(workout.error ? "--" : workout.vo2Max.formatted())
-                                    .font(.fieldRecordMeasure)
-                            } else {
-                                Text("--")
-                            }
-                            Text(" ml/kg/min")
-                                .font(.fieldRecordUnit)
-                        }
-                    }
+//                    VStack(alignment: .leading) {
+//                        Text("최대 산소 섭취량")
+//                            .font(.fieldRecordTitle)
+//                        HStack(alignment: .bottom, spacing: 0) {
+//                            if let workout = workout {
+//                                Text(workout.error ? "--" : workout.vo2Max.formatted())
+//                                    .font(.fieldRecordMeasure)
+//                            } else {
+//                                Text("--")
+//                            }
+//                            Text(" ml/kg/min")
+//                                .font(.fieldRecordUnit)
+//                        }
+//                    }
                 }
             }
             .padding(.vertical, 56)
